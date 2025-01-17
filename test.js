e77f06d1e331f8ddd992819c7d04069fe6d89e97
twinhitz <116257937+twinhitz@users.noreply.github.com>
2025-01-17 12:31:15 -0600

    Add getComp function to js

---
+++ test.js
@@ -0,0 +1,38 @@
+console.log("Hello World")
+
+// Return randomly string values of rock, paper, or scissors
+/* 
+
+
+*/
+
+
+function getComputerChoice() {
+    
+    let randomNumber = Math.floor(Math.random() * 3) +1;
+
+
+return randomNumber === 1 ? 'rock' : randomNumber === 2 ? 'paper': 'scissors';
+
+}
+
+
+
+ console.log(getComputerChoice())
+
+
+ function getHumanChoice() {
+
+   { let humChoice = prompt("Rock, paper, scissors?")
+        switch(humChoice){
+            
+            case "rock":
+            text = "";
+        }
+    }
+
+
+ }
+
+ console.log(getHumanChoice())
+
