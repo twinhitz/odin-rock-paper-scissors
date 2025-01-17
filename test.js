5c617346237bab2c4f24826b3fe37bafda0b57fa
twinhitz <116257937+twinhitz@users.noreply.github.com>
2025-01-17 14:17:15 -0600

    Add function playround and logic to js

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) +1;
 
 
-return randomNumber === 1 ? 'rock' : randomNumber === 2 ? 'paper': 'scissors';
+    return randomNumber === 1 ? 'rock' : randomNumber === 2 ? 'paper': 'scissors';
 
 }
 
@@ -24,7 +24,7 @@ return randomNumber === 1 ? 'rock' : randomNumber === 2 ? 'paper': 'scissors';
 
  function getHumanChoice() {
 
-    let userChoice = prompt('Rock, paper, scissors?', 'rock');
+    let userChoice = prompt('Rock, paper, scissors?');
 
      return userChoice;
 
@@ -32,4 +32,66 @@ return randomNumber === 1 ? 'rock' : randomNumber === 2 ? 'paper': 'scissors';
  }
 
  console.log(getHumanChoice())
+
+ let humanScore = 0
+ let computerScore = 0
+
+ function playRound(humanChoice, computerChoice) {
+
+ {   humanChoice = humanChoice.toLowerCase(); }
+
+    let a = 'rock' ,
+    b = 'paper',
+    c = 'scissors';
+
+ 
+    if (humanChoice === a && computerChoice === b) {
+
+        console.log('You lose! Paper beats Rock.')
+    } 
+    
+    else if (humanChoice === b && computerChoice === a)
+
+       { console.log('You win! Paper beats Rock.') }
+
+
+        if (humanChoice === a && computerChoice === c ) {
+
+            console.log('You Win! Rock beats Scissors.')
+        } 
+        
+        else if (humanChoice === c.toUpperCase && computerChoice === a )
+    
+         {  console.log('You lose!  Rock beats Scissors.')
+         }
+
+    if (humanChoice === c && computerChoice === b) {
+
+        console.log('You win! Scissors beats Paper.')
+    } 
+    
+    else if (humanChoice === b && computerChoice === c )
+
+        { console.log('You lose! Paper beats scissors.')
+        }
+
+    else if ( (humanChoice === a && computerChoice === a) || 
+        (humanChoice === b && computerChoice === b) ||
+        (humanChoice === c && computerChoice === c) ) {
+
+        console.log('It\'s a tie!')
+    }
+
+    else {
+        console.log('invalid')
+    }
+
+ }
+
+
+
+ const humanSelection = getHumanChoice();
+ const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
 
